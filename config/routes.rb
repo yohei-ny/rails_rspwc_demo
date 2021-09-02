82dff56f49b6462597745e130b19484a6aa07c80
ny-yohei <yo0314baseball@gmail.com>
2021-09-02 15:21:29 +0900

    rsec

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
