fa17ef43b5fb5e43aa6dcaf7aa7963caad8117ac
ny-yohei <yo0314baseball@gmail.com>
2021-09-05 21:55:42 +0900

    修正

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -11,18 +11,11 @@ RSpec.describe 'Listモデルのテスト', type: :model do
       it '空欄でないこと' do
         list.title = ''  
         #validationの設定の結果が返ってきたらfalseとなるため下記が通る
-
         is_expected.to eq false
         # false == false
       end
     end
-    it "practice" do
-      expect(1 + 13).to eq 2
-    end
+
   end
-  #   it '空欄でない場合' do
-  #     @list.title = Faker::Lorem.characters(number: 200)
-  #     is_expected.to eq true
-  #   end
-  # end
+
 end
